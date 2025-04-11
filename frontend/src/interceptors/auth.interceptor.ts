5a987ef899c0193e62e0117757a72e375d305729
Caoimhin <21307571@studentmail.ul.ie>
2025-04-11 16:55:27 +0100

    configuring updates to package.json hopefully finally for just another one and have it all up to shnuff

--- frontend/src/interceptors/auth.interceptor.ts
+++ frontend/src/interceptors/auth.interceptor.ts
@@ -1,11 +1,5 @@
 import { Injectable } from '@angular/core';
-import {
-  HttpRequest,
-  HttpHandler,
-  HttpEvent,
-  HttpInterceptor,
-  HttpErrorResponse
-} from '@angular/common/http';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpErrorResponse } from '@angular/common/http';
 import { Observable, from, throwError } from 'rxjs';
 import { catchError, switchMap } from 'rxjs/operators';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
